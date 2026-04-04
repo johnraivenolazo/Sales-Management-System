0562980d12622671c7f787f400bb2adfecffa22c
Jax-rgb <jacquescarigma@gmail.com>
2026-04-04 22:05:56 +0800

    chore(ui): add shared shadcn support files

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { cva } from "class-variance-authority";
 import { Tabs as TabsPrimitive } from "radix-ui"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils.js"
 
 function Tabs({
   className,
